ead5093ea3fa382cefc34ed18c36c1a8c4745001
Arvid Huvila <Arvid.Huvila@gmail.com>
2025-08-21 21:42:15 +0300

    OMGGGGG DATABASE UPDATE
    server now connects to mysql database

--- CR_server.js
+++ CR_server.js
@@ -3,10 +3,23 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const wss = new WebSocket.Server({ port: 8080});
 let clients = [];
 
+let con = mysql.createConnection({
+  host: "dbip", //if not in ip:port form. port not needed
+  port: dbport,
+  user: "dbuser",
+  password: "dbpassowrd",
+  database: "yourdb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Database connected");
+    
 let nextClientId = 1;
 
 wss.on('connection', (ws) => {
@@ -59,23 +72,22 @@ setInterval(() => {
 
 const app = express();
 
-const users = [{username: "admin", password: "$2b$12$mG.qkW8CqUUt7taY5BH4weOmuHY1smFXFY3EGjxls699EIZespALa", token: null}, {username: "Arvid", password: "$2b$12$L7yBP7kntXajmMI8nL7MB.OR5iSfNIwjWFFfGBtoS48Y0MEXijXZy", token: null}, {username: "Stefan", password: "$2b$12$.sXKARkEPAOuCDQBwBQgWOBG5yWos5yCEGlaCXWC9EurTEdE4nsE6", token: null}]; //admin : password
-
 app.use(express.json());
 app.use(cors({origin: '*'}));
-const SECRET_KEY = "your_secret_key";
+const SECRET_KEY = "your_secret_key"; //don't use "your_secret_key"
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    const user = users.find(user => user.username === username);
-    
-    if (user && bcrypt.compareSync(password, user.password)) {
-        const token = jwt.sign({ username }, SECRET_KEY);
-        res.json({ token: token });
-        user.token = token;
-    } else {
-        res.status(401).json({ message: "Invalid credentials" });
-    }
+    con.query("SELECT * FROM users WHERE name = ?", [username], (err, results) => {
+        if (err) throw err;
+        const user = results[0];
+        if (user && bcrypt.compareSync(password, user.hash)) {
+            const token = jwt.sign({ username }, SECRET_KEY);
+            res.json({ token: token });
+        } else {
+            res.status(401).json({ message: "Invalid credentials" });
+        }
+    });
 });
 
 app.get('/protected', (req, res) => {
@@ -93,4 +105,4 @@ app.get('/protected', (req, res) => {
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
-
+});
